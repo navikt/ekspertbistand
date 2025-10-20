c1a8bcc3b90219d52d1690c884e450acaab71367
Anders.Vik.Lysne <anders.vik.lysne@nav.no>
2025-10-20 15:00:28 +0200

    Legg til flere graphql filer. fiks generering av graphql klasser

---
+++ backend/src/main/kotlin/no/nav/ekspertbistand/services/notifikasjon/ProdusentClient.kt
@@ -0,0 +1,13 @@
+package no.nav.ekspertbistand.services.notifikasjon
+//
+//import io.ktor.client.HttpClient
+//import io.ktor.http.HttpStatusCode
+//
+//class ProdusentClient {
+//
+//    val client = HttpClient()
+//
+//    suspend fun opprettSak(sak: NySakInput): Result<HttpStatusCode> {
+//        client
+//    }
+//}
