37672f3bb1e32b4c71c37f9f7284099354a707a4
Anders.Vik.Lysne <anders.vik.lysne@nav.no>
2025-10-22 12:41:14 +0200

    Kopiert og tweaket produsentApiKlien fra permitteringsskjema

--- backend/src/main/kotlin/no/nav/ekspertbistand/services/notifikasjon/ProdusentClient.kt
+++ backend/src/main/kotlin/no/nav/ekspertbistand/services/notifikasjon/ProdusentClient.kt
@@ -1,13 +1,137 @@
 package no.nav.ekspertbistand.services.notifikasjon
-//
-//import io.ktor.client.HttpClient
-//import io.ktor.http.HttpStatusCode
-//
-//class ProdusentClient {
-//
-//    val client = HttpClient()
-//
-//    suspend fun opprettSak(sak: NySakInput): Result<HttpStatusCode> {
-//        client
-//    }
-//}
+
+import com.expediagroup.graphql.client.ktor.GraphQLKtorClient
+import io.ktor.client.request.*
+import no.nav.ekspertbistand.infrastruktur.NaisEnvironment
+import no.nav.ekspertbistand.infrastruktur.defaultHttpClient
+import no.nav.ekspertbistand.infrastruktur.logger
+import no.nav.ekspertbistand.services.entraId.EntraIdKlient
+import no.nav.ekspertbistand.services.notifikasjon.graphql.generated.ISO8601DateTime
+import no.nav.ekspertbistand.services.notifikasjon.graphql.generated.OpprettNyBeskjed
+import no.nav.ekspertbistand.services.notifikasjon.graphql.generated.OpprettNySak
+import no.nav.ekspertbistand.services.notifikasjon.graphql.generated.inputs.AltinnRessursMottakerInput
+import no.nav.ekspertbistand.services.notifikasjon.graphql.generated.inputs.MottakerInput
+import no.nav.ekspertbistand.services.notifikasjon.graphql.generated.opprettnybeskjed.DefaultNyBeskjedResultatImplementation
+import no.nav.ekspertbistand.services.notifikasjon.graphql.generated.opprettnybeskjed.DuplikatEksternIdOgMerkelapp
+import no.nav.ekspertbistand.services.notifikasjon.graphql.generated.opprettnybeskjed.NyBeskjedVellykket
+import no.nav.ekspertbistand.services.notifikasjon.graphql.generated.opprettnysak.DefaultNySakResultatImplementation
+import no.nav.ekspertbistand.services.notifikasjon.graphql.generated.opprettnysak.DuplikatGrupperingsid
+import no.nav.ekspertbistand.services.notifikasjon.graphql.generated.opprettnysak.DuplikatGrupperingsidEtterDelete
+import no.nav.ekspertbistand.services.notifikasjon.graphql.generated.opprettnysak.NySakVellykket
+import java.net.URI
+import no.nav.ekspertbistand.services.notifikasjon.graphql.generated.opprettnybeskjed.UgyldigMerkelapp as NyBeskjedUgyldigMerkelapp
+import no.nav.ekspertbistand.services.notifikasjon.graphql.generated.opprettnybeskjed.UgyldigMottaker as NyBeskjedUgyldigMottaker
+import no.nav.ekspertbistand.services.notifikasjon.graphql.generated.opprettnybeskjed.UkjentProdusent as NyBeskjedUkjentProdusent
+import no.nav.ekspertbistand.services.notifikasjon.graphql.generated.opprettnysak.UgyldigMerkelapp as NySakUgyldigMerkelapp
+import no.nav.ekspertbistand.services.notifikasjon.graphql.generated.opprettnysak.UgyldigMottaker as NySakUgyldigMottaker
+import no.nav.ekspertbistand.services.notifikasjon.graphql.generated.opprettnysak.UkjentProdusent as NySakUkjentProdusent
+import no.nav.ekspertbistand.services.notifikasjon.graphql.generated.opprettnysak.UkjentRolle as NySakUkjentRolle
+
+class ProdusentApiKlient(
+    private val entraIdClient: EntraIdKlient
+) {
+    private val url = URI("url").toURL()
+
+    private val log = logger()
+    private val client = GraphQLKtorClient(
+        url = url,
+        httpClient = defaultHttpClient()
+    )
+    private val ressursId = "eksperbistandRessurs"
+
+    private val mottaker = MottakerInput(
+        altinn = null,
+        altinnRessurs = AltinnRessursMottakerInput(ressursId = ressursId),
+        naermesteLeder = null
+    )
+
+    private suspend fun hentEntraIdToken(): String {
+        val scope = "api://${NaisEnvironment.clusterName}.fager.notifikasjon-produsent-api/.default"
+        return entraIdClient.getToken(scope)
+    }
+
+    suspend fun opprettNySak(
+        grupperingsid: String,
+        merkelapp: String,
+        virksomhetsnummer: String,
+        tittel: String,
+        lenke: String,
+        tidspunkt: ISO8601DateTime? = null
+    ) {
+        val token = hentEntraIdToken()
+        val resultat = client.execute(
+            OpprettNySak(
+                variables = OpprettNySak.Variables(
+                    grupperingsid,
+                    merkelapp,
+                    virksomhetsnummer,
+                    tittel,
+                    lenke,
+                    tidspunkt,
+                    mottaker
+                )
+            )
+        ) {
+            bearerAuth(token)
+        }
+
+        when (val nySak = resultat.data?.nySak) {
+            null -> throw Exception("Uventet feil: Ny sak er null, resultat: $resultat")
+
+            is NySakVellykket -> log.info("Opprettet ny sak {}", nySak.id)
+
+            is DuplikatGrupperingsid -> log.info("Sak finnes allerede. hopper over. {}", nySak.feilmelding)
+            is DuplikatGrupperingsidEtterDelete -> log.info("Sak finnes allerede. hopper over. {}", nySak.feilmelding)
+
+            is NySakUgyldigMerkelapp -> throw Exception(nySak.feilmelding)
+            is NySakUgyldigMottaker -> throw Exception(nySak.feilmelding)
+            is NySakUkjentProdusent -> throw Exception(nySak.feilmelding)
+            is NySakUkjentRolle -> throw Exception(nySak.feilmelding)
+
+            is DefaultNySakResultatImplementation -> throw Exception("Uventet feil: $resultat")
+        }
+    }
+
+    suspend fun opprettNyBeskjed(
+        grupperingsid: String,
+        merkelapp: String,
+        virksomhetsnummer: String,
+        tekst: String,
+        lenke: String,
+        tidspunkt: ISO8601DateTime? = null
+    ) {
+        val token = hentEntraIdToken()
+        val resultat = client.execute(
+            OpprettNyBeskjed(
+                variables = OpprettNyBeskjed.Variables(
+                    grupperingsid,
+                    merkelapp,
+                    virksomhetsnummer,
+                    tekst,
+                    lenke,
+                    tidspunkt,
+                    mottaker
+                )
+            )
+        ) {
+            bearerAuth(token)
+        }
+
+        when (val nyBeskjed = resultat.data?.nyBeskjed) {
+            null -> throw Exception("Uventet feil: Ny beskjed er null, resultat: $resultat")
+
+            is NyBeskjedVellykket -> log.info("Opprettet ny beskjed {}", nyBeskjed.id)
+
+            is DuplikatEksternIdOgMerkelapp -> log.info(
+                "Beskjed finnes allerede. hopper over. {}",
+                nyBeskjed.feilmelding
+            )
+
+            is NyBeskjedUgyldigMerkelapp -> throw Exception(nyBeskjed.feilmelding)
+            is NyBeskjedUgyldigMottaker -> throw Exception(nyBeskjed.feilmelding)
+            is NyBeskjedUkjentProdusent -> throw Exception(nyBeskjed.feilmelding)
+
+            is DefaultNyBeskjedResultatImplementation -> throw Exception("Uventet feil: $resultat")
+        }
+    }
+}
