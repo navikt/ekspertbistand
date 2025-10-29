9fc74ccdcbb33d81117da5d377779790c6ca86ad
Ken Gullaksen <ken.gullaksen@nav.no>
2025-10-29 16:30:03 +0100

    refactor application setup and improve test database handling

--- backend/src/test/kotlin/no/nav/ekspertbistand/event/EventMetricsTest.kt
+++ backend/src/test/kotlin/no/nav/ekspertbistand/event/EventMetricsTest.kt
@@ -25,33 +25,28 @@ import kotlin.time.ExperimentalTime
 
 @OptIn(ExperimentalTime::class, ExperimentalCoroutinesApi::class)
 class EventMetricsTest {
-    private lateinit var testDb: TestDatabase
-
-    @BeforeEach
-    fun setup() {
-        testDb = TestDatabase().cleanMigrate()
-    }
 
     @Test
     fun `eventQueueSizeGauge reflects database rows`() = runTest {
-        val dispatcher = UnconfinedTestDispatcher(testScheduler)
-        transaction {
-            QueuedEvents.insert {
-                it[QueuedEvents.status] = PENDING
-                it[QueuedEvents.eventData] = EventData.Foo("dummy1")
-                it[QueuedEvents.updatedAt] = CurrentTimestamp
+        TestDatabase().cleanMigrate().use {
+            val dispatcher = UnconfinedTestDispatcher(testScheduler)
+            transaction {
+                QueuedEvents.insert {
+                    it[QueuedEvents.status] = PENDING
+                    it[QueuedEvents.eventData] = EventData.Foo("dummy1")
+                    it[QueuedEvents.updatedAt] = CurrentTimestamp
+                }
+                QueuedEvents.insert {
+                    it[QueuedEvents.status] = PENDING
+                    it[QueuedEvents.eventData] = EventData.Foo("dummy2")
+                    it[QueuedEvents.updatedAt] = CurrentTimestamp
+                }
+                QueuedEvents.insert {
+                    it[QueuedEvents.status] = PROCESSING
+                    it[QueuedEvents.eventData] = EventData.Foo("dummy3")
+                    it[QueuedEvents.updatedAt] = CurrentTimestamp
+                }
             }
-            QueuedEvents.insert {
-                it[QueuedEvents.status] = PENDING
-                it[QueuedEvents.eventData] = EventData.Foo("dummy2")
-                it[QueuedEvents.updatedAt] = CurrentTimestamp
-            }
-            QueuedEvents.insert {
-                it[QueuedEvents.status] = PROCESSING
-                it[QueuedEvents.eventData] = EventData.Foo("dummy3")
-                it[QueuedEvents.updatedAt] = CurrentTimestamp
-            }
-        }
 
         val meterRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
         with(EventMetrics(dispatcher, meterRegistry)) {
@@ -59,129 +54,138 @@ class EventMetricsTest {
             delay(60.seconds)
             updateGaugeJob.cancel()
 
-            meterRegistry
-                .get("eventqueue.size")
-                .tag("status", PENDING.name)
-                .gauge().let {
-                    assertEquals(2.0, it.value())
-                }
+                meterRegistry
+                    .get("eventqueue.size")
+                    .tag("status", PENDING.name)
+                    .gauge().let {
+                        assertEquals(2.0, it.value())
+                    }
 
-            meterRegistry
-                .get("eventqueue.size")
-                .tag("status", PROCESSING.name)
-                .gauge().let {
-                    assertEquals(1.0, it.value())
-                }
+                meterRegistry
+                    .get("eventqueue.size")
+                    .tag("status", PROCESSING.name)
+                    .gauge().let {
+                        assertEquals(1.0, it.value())
+                    }
+            }
+
+
         }
     }
 
     @Test
     fun `eventLogSizeGauge reflects database rows`() = runTest {
-        val dispatcher = UnconfinedTestDispatcher(testScheduler)
-        transaction {
-            EventLog.insert {
-                it[EventLog.id] = 1L
-                it[EventLog.status] = COMPLETED_WITH_ERRORS
-                it[EventLog.eventData] = EventData.Foo("dummy1")
-                it[EventLog.updatedAt] = CurrentTimestamp
-            }
-            EventLog.insert {
-                it[EventLog.id] = 2L
-                it[EventLog.status] = COMPLETED
-                it[EventLog.eventData] = EventData.Foo("dummy2")
-                it[EventLog.updatedAt] = CurrentTimestamp
-            }
-            EventLog.insert {
-                it[EventLog.id] = 3L
-                it[EventLog.status] = COMPLETED
-                it[EventLog.eventData] = EventData.Foo("dummy3")
-                it[EventLog.updatedAt] = CurrentTimestamp
-            }
-        }
-
-        val meterRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
-        with(EventMetrics(dispatcher, meterRegistry)) {
-            val updateGaugeJob = launch { updateGaugesProcessingLoop(Clock.System) }
-            delay(60.seconds)
-            updateGaugeJob.cancel()
-
-            meterRegistry
-                .get("eventlog.size")
-                .tag("status", COMPLETED.name)
-                .gauge().let {
-                    assertEquals(2.0, it.value())
+        TestDatabase().cleanMigrate().use {
+            val dispatcher = UnconfinedTestDispatcher(testScheduler)
+            transaction {
+                EventLog.insert {
+                    it[EventLog.id] = 1L
+                    it[EventLog.status] = COMPLETED_WITH_ERRORS
+                    it[EventLog.eventData] = EventData.Foo("dummy1")
+                    it[EventLog.updatedAt] = CurrentTimestamp
                 }
-
-            meterRegistry
-                .get("eventlog.size")
-                .tag("status", COMPLETED_WITH_ERRORS.name)
-                .gauge().let {
-                    assertEquals(1.0, it.value())
+                EventLog.insert {
+                    it[EventLog.id] = 2L
+                    it[EventLog.status] = COMPLETED
+                    it[EventLog.eventData] = EventData.Foo("dummy2")
+                    it[EventLog.updatedAt] = CurrentTimestamp
                 }
+                EventLog.insert {
+                    it[EventLog.id] = 3L
+                    it[EventLog.status] = COMPLETED
+                    it[EventLog.eventData] = EventData.Foo("dummy3")
+                    it[EventLog.updatedAt] = CurrentTimestamp
+                }
+            }
+
+            val meterRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+            with(EventMetrics(dispatcher, meterRegistry)) {
+                val updateGaugeJob = launch { updateGaugesProcessingLoop(Clock.System) }
+                delay(60.seconds)
+                updateGaugeJob.cancel()
+
+                meterRegistry
+                    .get("eventlog.size")
+                    .tag("status", COMPLETED.name)
+                    .gauge().let {
+                        assertEquals(2.0, it.value())
+                    }
+
+                meterRegistry
+                    .get("eventlog.size")
+                    .tag("status", COMPLETED_WITH_ERRORS.name)
+                    .gauge().let {
+                        assertEquals(1.0, it.value())
+                    }
+            }
         }
     }
 
     @Test
     fun `processingEventsAgeGauge reflects correct age buckets`() = runTest {
-        val dispatcher = UnconfinedTestDispatcher(testScheduler)
-        val now = Clock.System.now()
-        transaction {
-            QueuedEvents.insert {
-                it[QueuedEvents.eventData] = EventData.Foo("dummy")
-                it[QueuedEvents.status] = PROCESSING
-                it[QueuedEvents.updatedAt] = now.minus(30.seconds) // <1m
+        TestDatabase().cleanMigrate().use {
+            val dispatcher = UnconfinedTestDispatcher(testScheduler)
+            val now = Clock.System.now()
+            transaction {
+                QueuedEvents.insert {
+                    it[QueuedEvents.eventData] = EventData.Foo("dummy")
+                    it[QueuedEvents.status] = PROCESSING
+                    it[QueuedEvents.updatedAt] = now.minus(30.seconds) // <1m
+                }
+                QueuedEvents.insert {
+                    it[QueuedEvents.eventData] = EventData.Foo("dummy")
+                    it[QueuedEvents.status] = PROCESSING
+                    it[QueuedEvents.updatedAt] = now.minus(4.minutes) // <5m
+                }
+                QueuedEvents.insert {
+                    it[QueuedEvents.eventData] = EventData.Foo("dummy")
+                    it[QueuedEvents.status] = PROCESSING
+                    it[QueuedEvents.updatedAt] = now.minus(10.minutes) // <15m
+                }
+                QueuedEvents.insert {
+                    it[QueuedEvents.eventData] = EventData.Foo("dummy")
+                    it[QueuedEvents.status] = PROCESSING
+                    it[QueuedEvents.updatedAt] = now.minus(35.minutes) // >30m
+                }
             }
-            QueuedEvents.insert {
-                it[QueuedEvents.eventData] = EventData.Foo("dummy")
-                it[QueuedEvents.status] = PROCESSING
-                it[QueuedEvents.updatedAt] = now.minus(4.minutes) // <5m
-            }
-            QueuedEvents.insert {
-                it[QueuedEvents.eventData] = EventData.Foo("dummy")
-                it[QueuedEvents.status] = PROCESSING
-                it[QueuedEvents.updatedAt] = now.minus(10.minutes) // <15m
-            }
-            QueuedEvents.insert {
-                it[QueuedEvents.eventData] = EventData.Foo("dummy")
-                it[QueuedEvents.status] = PROCESSING
-                it[QueuedEvents.updatedAt] = now.minus(35.minutes) // >30m
-            }
-        }
 
-        val meterRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
-        with(EventMetrics(dispatcher, meterRegistry)) {
-            val updateGaugeJob = launch { updateGaugesProcessingLoop(object : Clock {
-                override fun now() = now
-            }) }
-            delay(60.seconds)
-            updateGaugeJob.cancel()
+            val meterRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+            with(EventMetrics(dispatcher, meterRegistry)) {
+                val updateGaugeJob = launch {
+                    updateGaugesProcessingLoop(object : Clock {
+                        override fun now() = now
+                    })
+                }
+                delay(60.seconds)
+                updateGaugeJob.cancel()
 
-            meterRegistry.get("eventqueue.age")
-                .tag("age", "<1m")
-                .gauge().let {
-                    assertEquals(1.0, it.value())
+                meterRegistry.get("eventqueue.age")
+                    .tag("age", "<1m")
+                    .gauge().let {
+                        assertEquals(1.0, it.value())
 
-                }
-            meterRegistry.get("eventqueue.age")
-                .tag("age", "<5m")
-                .gauge().let {
-                    assertEquals(1.0, it.value())
-                }
-            meterRegistry.get("eventqueue.age")
-                .tag("age", "<15m")
-                .gauge().let {
-                    assertEquals(1.0, it.value())
-                }
-            meterRegistry.get("eventqueue.age")
-                .tag("age", "<30m")
-                .gauge().let {
-                    assertEquals(0.0, it.value())
-                }
-            meterRegistry.get("eventqueue.age")
-                .tag("age", ">30m")
-                .gauge().let {
-                    assertEquals(1.0, it.value())
-                }
+                    }
+                meterRegistry.get("eventqueue.age")
+                    .tag("age", "<5m")
+                    .gauge().let {
+                        assertEquals(1.0, it.value())
+                    }
+                meterRegistry.get("eventqueue.age")
+                    .tag("age", "<15m")
+                    .gauge().let {
+                        assertEquals(1.0, it.value())
+                    }
+                meterRegistry.get("eventqueue.age")
+                    .tag("age", "<30m")
+                    .gauge().let {
+                        assertEquals(0.0, it.value())
+                    }
+                meterRegistry.get("eventqueue.age")
+                    .tag("age", ">30m")
+                    .gauge().let {
+                        assertEquals(1.0, it.value())
+                    }
+            }
         }
     }
 }
