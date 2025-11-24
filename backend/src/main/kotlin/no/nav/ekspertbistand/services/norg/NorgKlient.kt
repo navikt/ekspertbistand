df826d5cb320985ce4cffdd2892ff827b87d4225
Anders.Vik.Lysne <anders.vik.lysne@nav.no>
2025-11-24 09:53:31 +0100

    Legg til NorgKlient

---
+++ backend/src/main/kotlin/no/nav/ekspertbistand/services/norg/NorgKlient.kt
@@ -0,0 +1,54 @@
+package no.nav.ekspertbistand.services.norg
+
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonIgnoreUnknownKeys
+import no.nav.ekspertbistand.infrastruktur.basedOnEnv
+
+class NorgKlient(
+    private val httpClient: HttpClient
+) {
+    suspend fun hentBehandlendeEnhet(kommuneNummer: String): Norg2Enhet? {
+        val norg2ResponseListe = try {
+            httpClient.post("$baseUrl/norg2/api/v1/arbeidsfordeling/enheter/bestmatch")
+            {
+                contentType(ContentType.Application.Json)
+                setBody(Norg2Request(kommuneNummer))
+            }.body<List<Norg2Enhet>>()
+        } catch (e: Exception) {
+            throw RuntimeException("Hente behandlendeEnhet for $kommuneNummer feilet", e)
+        }
+
+        return norg2ResponseListe.firstOrNull { it.status == "Aktiv" }
+    }
+
+
+    companion object {
+        val baseUrl = basedOnEnv(
+            prod = { "https://norg2.prod-fss-pub.nais.io" },
+            other = { "https://norg2.dev-fss-pub.nais.io" },
+        )
+
+        // TODO: Skal denne brukes dersom vi får tom liste fra Norg?
+        private const val OSLO_ARBEIDSLIVSENTER_KODE = "0391"
+    }
+}
+
+@OptIn(ExperimentalSerializationApi::class)
+@Serializable
+@JsonIgnoreUnknownKeys
+data class Norg2Enhet(
+    val enhetNr: String,
+    val status: String,
+)
+
+@Serializable
+private data class Norg2Request(
+    val geografiskOmraade: String,
+) {
+    val tema: String = "" //TODO: Hva er tema fro ekspertbistand?
+}
