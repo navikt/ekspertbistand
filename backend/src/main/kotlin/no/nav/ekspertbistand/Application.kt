213b89070aff16112c7e1572cbe3f85dc319eb6b
Anders.Vik.Lysne <anders.vik.lysne@nav.no>
2026-01-14 10:36:22 +0100

    nuke database

--- backend/src/main/kotlin/no/nav/ekspertbistand/Application.kt
+++ backend/src/main/kotlin/no/nav/ekspertbistand/Application.kt
@@ -56,7 +56,7 @@ fun main() {
     ktorServer {
         dependencies {
             provide<Database> {
-                //dbConfig.destroyExistingDatabase()
+                dbConfig.destroyExistingDatabase()
                 dbConfig.flywayAction {
                     migrate()
                 }
