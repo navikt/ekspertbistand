d95eb81b504a2cba7a25bcb8929fc5c949f1d704
Anders.Vik.Lysne <anders.vik.lysne@nav.no>
2026-01-14 11:56:35 +0100

    remove nuke on deploy

--- backend/src/main/kotlin/no/nav/ekspertbistand/Application.kt
+++ backend/src/main/kotlin/no/nav/ekspertbistand/Application.kt
@@ -55,7 +55,7 @@ fun main() {
     ktorServer {
         dependencies {
             provide<Database> {
-                dbConfig.destroyExistingDatabase()
+//                dbConfig.destroyExistingDatabase()
                 dbConfig.flywayAction {
                     migrate()
                 }
