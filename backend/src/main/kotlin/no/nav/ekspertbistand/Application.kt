0e45a5b5ca587bb6e55f6dd5d59dc1757df4cc1c
Anders.Vik.Lysne <anders.vik.lysne@nav.no>
2026-01-27 13:23:41 +0100

    fjern nuke av db

--- backend/src/main/kotlin/no/nav/ekspertbistand/Application.kt
+++ backend/src/main/kotlin/no/nav/ekspertbistand/Application.kt
@@ -60,7 +60,7 @@ fun main() {
     ktorServer {
         dependencies {
             provide<Database> {
-                dbConfig.destroyExistingDatabase()
+//                dbConfig.destroyExistingDatabase()
                 dbConfig.flywayAction {
                     migrate()
                 }
